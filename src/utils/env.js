baf95fc75facfc31c47423b5766e7bdddd6d4446
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2023-01-27 17:08:43 +0100

    minor optimizations

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,8 @@
+import { Platform } from "react-native";
 const liveHost = "https://us-central1-mealstogo-1dd22.cloudfunctions.net";
 const localHost = "http://localhost:5001/mealstogo-1dd22/us-central1";
+
+export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const host = isDevelopment ? localHost : liveHost;
+export const isMock = true;
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
